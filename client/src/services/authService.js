8ec08c3aef9468a1ba3f8d937c59e62de2abd17a
Anuj Dhurve <sanujdhurve12@gmail.com>
2026-06-25 12:33:01 +0530

    removed the /api from /api/auth routes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,23 +1,23 @@
 import api from './api';
 
 export const register = async (email, password) => {
-  const { data } = await api.post('/api/auth/register', { email, password });
+  const { data } = await api.post('/auth/register', { email, password });
   localStorage.setItem('accessToken', data.accessToken);
   return data;
 };
 
 export const login = async (email, password) => {
-  const { data } = await api.post('/api/auth/login', { email, password });
+  const { data } = await api.post('/auth/login', { email, password });
   localStorage.setItem('accessToken', data.accessToken);
   return data;
 };
 
 export const logout = async () => {
-  await api.post('/api/auth/logout');
+  await api.post('/auth/logout');
   localStorage.removeItem('accessToken');
 };
 
 export const getMe = async () => {
-  const { data } = await api.get('/api/auth/me');
+  const { data } = await api.get('/auth/me');
   return data.user;
 };
